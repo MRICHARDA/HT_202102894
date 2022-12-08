ed7dc87966dd6d22497e90f2c19f93446249dba6
Richard <mrichardarana@gmail.com>
2022-12-07 22:38:52 -0600

    Ya funciona :D

--- HT_202102894.py
+++ HT_202102894.py
@@ -1,6 +1,4 @@
-n1 = input("¿Dime el primer número?")
-n2 = input("¿Dime el segundo número?")
-
-print (n1+n2)
-
-print ("La suma de los dos números es: " + str(n1+n2))
+n1 = int(input("¿Dime el primer número?"))
+n2 = int(input("¿Dime el segundo número?"))
+suma = n1 + n2
+print("La suma es: ", suma)
